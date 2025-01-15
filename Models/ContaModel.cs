38139579b32762190b3455d5307cd63084efbdac
Willians <williansvpf@gmail.com>
2025-01-14 21:03:00 -0300

    pequenos ajustes

--- Models/ContaModel.cs
+++ Models/ContaModel.cs
@@ -2,10 +2,10 @@ namespace CadastroContatos.Models
 {
     public class ContaModel
     {
-        public int IdConta { get; set; }
+        public int? IdConta { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string Salt { get; set; }
-        public int IdUsuario { get; set; }
+        public string? Salt { get; set; }
+        public int? IdUsuario { get; set; }
     }
 }
