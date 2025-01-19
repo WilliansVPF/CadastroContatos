61600415d609088b3aa39c3d92e44b5dec2d17ae
Willians <williansvpf@gmail.com>
2025-01-19 15:28:21 -0300

    criado controller para area de contatos

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -1,13 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using CadastroContatos.Models;
+using CadastroContatos.Interfaces;
+using CadastroContatos.Services;
+using CadastroContatos.Filters;
 
 namespace CadastroContatos.Controllers
 {
+    [ValidarSessao]
     public class ContatoController : Controller
     {
+        private readonly ISessao _sessao;
+
+        public ContatoController(ISessao sessao)
+        {
+            _sessao = sessao;
+        }
         public IActionResult Index()
         {
-            return View();
+            UsuarioModel usuario = _sessao.BuscarSessao();
+            return View(usuario);
+        }
+
+        public IActionResult Sair()
+        {
+            _sessao.RemoverSessao();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult NovoContato()
+        {
+            UsuarioModel usuario = _sessao.BuscarSessao();
+            return View(usuario);
         }
         
     }
